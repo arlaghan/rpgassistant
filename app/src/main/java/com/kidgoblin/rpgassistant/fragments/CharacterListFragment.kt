2ed7b62c5f2965eec4a26bf9c096c2e3d9304413
arlaghan <arlaghan@gmail.com>
2021-07-17 11:02:36 -0700

    Clean-up

--- app/src/main/java/com/kidgoblin/rpgassistant/fragments/CharacterListFragment.kt
+++ app/src/main/java/com/kidgoblin/rpgassistant/fragments/CharacterListFragment.kt
@@ -7,10 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.kidgoblin.rpgassistant.MainActivity
 import com.kidgoblin.rpgassistant.R
 import com.kidgoblin.rpgassistant.Utils
@@ -18,18 +15,17 @@ import com.kidgoblin.rpgassistant.adapters.CharacterListAdapter
 import com.kidgoblin.rpgassistant.databinding.FragmentCharacterListBinding
 import com.kidgoblin.rpgassistant.models.Character
 
-
 class CharacterListFragment : Fragment() {
     private var _binding: FragmentCharacterListBinding? = null
     private val binding get() = _binding!!
-    private var characterList = ArrayList<Character>();
+    private var characterList = ArrayList<Character>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCharacterListBinding.inflate(inflater, container, false);
+        _binding = FragmentCharacterListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -43,7 +39,7 @@ class CharacterListFragment : Fragment() {
 
         //Set the FAB to open character create
         val fab = (activity as MainActivity?)!!.findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener { _ ->
+        fab.setOnClickListener {
             findNavController().navigate(R.id.action_CharacterListFragment_to_CharacterCreateFragment)
         }
         fab.show()
@@ -53,11 +49,10 @@ class CharacterListFragment : Fragment() {
         binding.characterList.adapter = CharacterListAdapter(requireContext(), characterList)
         binding.characterList.layoutManager = LinearLayoutManager(requireContext())
 
-        if(characterList.size == 0) {
+        if (characterList.size == 0) {
             //No characters, show the placeholder text
             binding.message.visibility = View.VISIBLE
-        }
-        else {
+        } else {
             //Some characters present, show list
             binding.characterList.visibility = View.VISIBLE
         }
